564ad81699688e2794c4720134db33b4f01074b0
smagucha <smagucha@gmail.com>
2023-06-26 04:07:04 +0300

    add nore urls

--- school/parent/signals.py
+++ school/parent/signals.py
@@ -1,4 +1,3 @@
-from django.db.models import signals
 from django.dispatch import receiver
 from useraccounts.models import MyUser
 from django.contrib.auth.models import Group
@@ -6,8 +5,23 @@ from django.db.models.signals import post_save
 from .models import Parent
 
 
-@receiver(post_save, sender=MyUser)
-def update_profile(sender, created, instance, **kwargs):
-    if created:
+# @receiver(post_save, sender=MyUser)
+# def update_profile(sender, created, instance, **kwargs):
+#     if created:
+#         parent = Parent.objects.create(user=instance)
+#         parent.save()
+# #
+
+
+@receiver(post_save, sender=MyUser)  # Update with the actual User model name
+def assign_parent_group(sender, instance, created, **kwargs):
+    if created and instance.groups.filter(name="Parent").exists():
         parent = Parent.objects.create(user=instance)
         parent.save()
+
+
+# @receiver(post_save, sender=MyUser)  # Update with the actual User model name
+# def assign_teacher_group(sender, instance, created, **kwargs):
+#     if created and instance.groups.filter(name="Teacher").exists():
+#         # User belongs to the "Teacher" group
+#         # Perform actions specific to the "Teacher" group
